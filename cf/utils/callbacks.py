1e3fcaf04c1e85326092ef87fa4a2a52b966af3e
2Mw <abd771567@gmail.com>
2022-05-27 12:04:30 +0800

    1. Compatible more for tensorflow 2.3.0. 2. Optimized callbacks details.

--- cf/utils/callbacks.py
+++ cf/utils/callbacks.py
@@ -2,24 +2,27 @@ import json
 import os.path
 
 import numpy as np
-from keras.utils import tf_utils
 from keras.callbacks import Callback
 import pickle
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
 class AbnormalAUC(Callback):
-    def __init__(self, threshold=0.8, steps: int = 0):
+    def __init__(self, threshold=0.8, steps: int = 0, directory: str = ''):
         """
         用于训练fit过程早停，auc大于threshold就停止训练
 
         :param threshold: train auc的最大值
         :param steps: 大于某个steps才生效
+        :param directory: Use to save model for lower tf version.
         """
         super(AbnormalAUC, self).__init__()
         self._supports_tf_logs = True
         self.threshold = threshold
         self.steps = steps
+        self.directory = directory
+        self.low_tf_version = float(tf.__version__.replace('.', '')) < 240
 
     def on_batch_end(self, batch, logs=None):
         logs = logs or {}
@@ -28,6 +31,8 @@ class AbnormalAUC(Callback):
             auc = auc.numpy().item()
             if auc > self.threshold and batch > self.steps:
                 self.model.stop_training = True
+                if self.low_tf_version:
+                    self.model.save_weights(os.path.join(self.directory, f'weights.999-{self.threshold}.hdf5'))
 
 
 class MetricsMonitor(Callback):
@@ -50,6 +55,8 @@ class MetricsMonitor(Callback):
         self.metric = metric
         self.sample_step = sample_step
         self.records = []
+        self.epoch = -1
+        self.best_epoch = 0
 
     def on_batch_end(self, batch, logs=None):
         logs = logs or {}
@@ -58,11 +65,13 @@ class MetricsMonitor(Callback):
             value = value.numpy().item()
             if not np.isnan(value) and not np.isinf(value) and self.op(value, self.best_value):
                 self.best_value = value
+                self.best_epoch = self.epoch
             if self.directory != '' and batch % self.sample_step == 0:
                 self.records.append(value)
 
     def on_epoch_begin(self, epoch, logs=None):
         self.records = []
+        self.epoch += 1
 
     def on_epoch_end(self, epoch, logs=None):
         if self.directory != '' and os.path.exists(self.directory):
@@ -72,6 +81,8 @@ class MetricsMonitor(Callback):
             plt.plot(self.records)
             # plt.legend(f'epoch-{epoch}')
             plt.title(f'epoch-{epoch}')
+            if epoch == 0:
+                plt.ylim(0.785, 0.810)
             plt.xlabel('steps')
             plt.ylabel(self.metric)
             plt.savefig(os.path.join(self.directory, f'{self.metric}_curve_{epoch}.png'))
@@ -81,4 +92,4 @@ class MetricsMonitor(Callback):
         if self.directory != '' and os.path.exists(self.directory):
             with open(os.path.join(self.directory, f'{self.metric}_{self.mode}.json'), 'w') as f:
                 json.dump(res, f)
-        print(f'{self.metric}_{self.mode} best value is {self.best_value}')
+        print(f'{self.metric}_{self.mode} best value is {self.best_value} in epoch-{self.best_epoch}')
