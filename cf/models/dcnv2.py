fix: the wrong way of preprocess the numerical data in different shape. feat: add different embedding store ways. refactor: the model package of getting embedding and input_x, the way of summary.

cf/models/dcnv2.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.models import Model
 from keras.layers import Embedding, Dense, Input
-from keras.regularizers import l2
+from cf.models.base import *
 from cf.layers import crossnet, mlp
 
 
@@ -19,37 +19,18 @@ class DCNv2(Model):
         self.cross = crossnet.CrossNetMix(model_cfg['low_rank'], model_cfg['num_experts'], model_cfg['cross_layers'],
                                           model_cfg['l2_reg_cross'], initializer=keras.initializers.he_normal)
 
-        self.ebd = {
-            f['name']: Embedding(
-                input_dim=f['feature_num'],
-                input_length=1,
-                output_dim=f['dim'],
-                embeddings_initializer='random_normal',
-                embeddings_regularizer=l2(model_cfg['l2_reg_embedding'])
-            )
-            for f in feature_column
-        }
+        self.embedding_dim = model_cfg['embedding_dim']
+        self.numeric_same = model_cfg['numeric_same_dim']
+        self.ebd = get_embedding(self, feature_column, self.embedding_dim, self.numeric_same, model_cfg['embedding_device'])
         # self.final_dense = Dense(1, model_cfg['activation'])
         self.final_dense = Dense(1, activation=None)
 
-    def summary(self,
-                line_length=None,
-                positions=None,
-                print_fn=None,
-                expand_nested=False,
-                show_trainable=False):
-        inputs = {
-            f['name']: Input(shape=(), dtype=tf.float32, name=f['name'])
-            for f in self.feature_column
-        }
-        model = Model(inputs, outputs=self.call(inputs))
-        if len(self.directory) > 0:
-            keras.utils.plot_model(model, os.path.join(self.directory, "model.png"), show_shapes=True)
-        model.summary()
+    def summary(self, line_length=None, positions=None, print_fn=None, expand_nested=False, show_trainable=False):
+        model_summary(self, self.feature_column, self.directory)
 
     def call(self, inputs, training=None, mask=None):
         # 对于 类别型数据使用 embedding，对于数值型数值不使用 embedding
-        x = tf.concat([self.ebd[f](v) if f[0] == 'C' else tf.expand_dims(v, 1) for f, v in inputs.items()], axis=1)
+        x = form_x(inputs, self.ebd, self.numeric_same)
         cross_out = self.cross(x)
         dnn_out = self.mlp(x)
         total_x = tf.concat([cross_out, dnn_out], axis=-1)

cf/run/base.py
@@ -24,13 +24,13 @@ def initModel(model_name: str, cfg, feature_columns, directory, weights: str = '
     """
     train_config = cfg['train']
     model_config = cfg['model']
-    mirrored_strategy = tf.distribute.MirroredStrategy()
-    with mirrored_strategy.scope():
-        ins = pool.get(model_name)
-        model = ins(feature_columns, cfg, directory)
-        model.summary()
-        optimizer = get_optimizer(train_config['optimizer'], train_config['lr'])
-        model.compile(loss=train_config['loss'], optimizer=optimizer, metrics=model_config['metrics'])
+    # mirrored_strategy = tf.distribute.MirroredStrategy()
+    # with mirrored_strategy.scope():
+    ins = pool.get(model_name)
+    model = ins(feature_columns, cfg, directory)
+    model.summary()
+    optimizer = get_optimizer(train_config['optimizer'], train_config['lr'])
+    model.compile(loss=train_config['loss'], optimizer=optimizer, metrics=model_config['metrics'])
     if weights == '' or weights is None:
         return model
     if os.path.exists(weights):