5063ce13c010ecabc77b9c6ca526e07db1b75560
2Mw <abd771567@gmail.com>
2022-06-03 16:39:17 +0800

    chore: optimized cowclip memory use.

--- cf/models/cowclip.py
+++ cf/models/cowclip.py
@@ -1,3 +1,4 @@
+import gc
 import math
 
 from keras.models import Model
@@ -24,11 +25,12 @@ class Cowclip(Model):
         def should_record():
             return tf.equal(tf.math.floormod(self.cur_step, self.log_freq), 0)
 
-        record_env = tf.summary.record_if(should_record)
-        tf.summary.experimental.set_step(self.cur_step)
+        if self.log:
+            record_env = tf.summary.record_if(should_record)
+            tf.summary.experimental.set_step(self.cur_step)
 
         # assist vars
-        name_to_layer = {x.name: x for x in self.trainable_variables}
+        # name_to_layer = {x.name: x for x in self.trainable_variables}
         # 前者保存 id，后者保存出现的次数
         uniq_ids, uniq_cnt = dict(), dict()
         for k, v in data[0].items():
@@ -39,11 +41,10 @@ class Cowclip(Model):
                 uniq_cnt[k] = count
 
         # main
-        x, y = data
         with tf.GradientTape() as tape:
-            y_pred = self(x, training=True)
+            y_pred = self(data[0], training=True)
             # loss = self.compiled_loss(y, y_pred)
-            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+            loss = self.compiled_loss(data[1], y_pred, regularization_losses=self.losses)
 
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
@@ -53,53 +54,59 @@ class Cowclip(Model):
         #     x.name: g for x, g in zip(self.trainable_variables, gradients)
         # }
         # 找到所有的 embedding 在trainable_vars中的索引
-        embed_index = [i for i, x in enumerate(trainable_vars) if "embeddings" in x.name]
-        # 找到所有的 dense 在trainable_vars中的索引
-        dense_index = [i for i in range(len(trainable_vars)) if i not in embed_index]
-        embed_vars = [trainable_vars[i] for i in embed_index]
-        dense_vars = [trainable_vars[i] for i in dense_index]
-        embed_gradients = [gradients[i] for i in embed_index]
-        dense_gradients = [gradients[i] for i in dense_index]
+        # embed_index = [i for i, x in enumerate(trainable_vars) if "embeddings" in x.name]
+        # # 找到所有的 dense 在trainable_vars中的索引
+        # dense_index = [i for i in range(len(trainable_vars)) if i not in embed_index]
+        # embed_vars = [trainable_vars[i] for i in embed_index]
+        # dense_vars = [trainable_vars[i] for i in dense_index]
+        # embed_gradients = [gradients[i] for i in embed_index]
+        # dense_gradients = [gradients[i] for i in dense_index]
 
         # CowClip
         if self.clip > 0:
             lower_bound = self.clip * math.sqrt(self.sparse_embed_dim) * self.bound
-            embed_gradients_clipped = []
-            for w, g in zip(embed_vars, embed_gradients):
-                if 'linear' in w.name:
-                    embed_gradients_clipped.append(g)
-                    continue
-                prefix = "sparse_emb_"
-                col_name = w.name[w.name.find(prefix) + len(prefix): w.name.find("/")]
+            # embed_gradients_clipped = []
+            prefix = "sparse_emb_"
+            for i in range(len(trainable_vars)):
+                w = trainable_vars[i]
+                if 'embeddings' in w.name and prefix in w.name:
+                    col_name = w.name[w.name.find(prefix) + len(prefix): w.name.find("/")]
+                    gradients[i] = self.cow_clip(w, gradients[i], ratio=self.clip, ids=uniq_ids[col_name],
+                                                 cnts=uniq_cnt[col_name], min_w=lower_bound)
+            # for w, g in zip(embed_vars, embed_gradients):
+            #     if 'linear' in w.name:
+            #         embed_gradients_clipped.append(g)
+            #         continue
+            #     col_name = w.name[w.name.find(prefix) + len(prefix): w.name.find("/")]
+            #
+            #     g_clipped = self.cow_clip(w, g, ratio=self.clip, ids=uniq_ids[col_name], cnts=uniq_cnt[col_name],
+            #                               min_w=lower_bound)
+            #     embed_gradients_clipped.append(g_clipped)
+            #
+            # embed_gradients = embed_gradients_clipped
 
-                g_clipped = self.cow_clip(w, g, ratio=self.clip, ids=uniq_ids[col_name], cnts=uniq_cnt[col_name],
-                                          min_w=lower_bound)
-                embed_gradients_clipped.append(g_clipped)
-
-            embed_gradients = embed_gradients_clipped
-
-        gradients = embed_gradients + dense_gradients
-        # Attention 原来的代码可能会导致 grad 和 feature 不对应，因此在此处修改
-        new_grad = []
-        e, d = 0, 0
-        for i in range(len(trainable_vars)):
-            if i in embed_index:
-                new_grad.append(embed_gradients[e])
-                e += 1
-            elif i in dense_index:
-                new_grad.append(dense_gradients[d])
-                d += 1
+        # gradients = embed_gradients + dense_gradients
+        # # Attention 原来的代码可能会导致 grad 和 feature 不对应，因此在此处修改
+        # new_grad = []
+        # e, d = 0, 0
+        # for i in range(len(trainable_vars)):
+        #     if i in embed_index:
+        #         new_grad.append(embed_gradients[e])
+        #         e += 1
+        #     elif i in dense_index:
+        #         new_grad.append(dense_gradients[d])
+        #         d += 1
 
         # update
-        self.optimizer.apply_gradients(zip(new_grad, trainable_vars))
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
         # =====
         # Logging
         # =====
-        with record_env:
-            if self.log:
-                embed_gradients = [gradients[i] for i in embed_index]
-                dense_gradients = [gradients[i] for i in dense_index]
+        if self.log:
+            with record_env:
+                # embed_gradients = [gradients[i] for i in embed_index]
+                # dense_gradients = [gradients[i] for i in dense_index]
                 specs = None
                 if hasattr(self.optimizer, 'optimizer_specs'):
                     specs = self.optimizer.optimizer_specs
@@ -108,36 +115,39 @@ class Cowclip(Model):
                 tf.summary.scalar("lr/dense", specs[1]['optimizer']._decayed_lr('float32'))
                 tf.summary.scalar("loss/loss", loss)
                 tf.summary.scalar("global_norm/global", tf.linalg.global_norm(gradients))
-                tf.summary.scalar("global_norm/dense", tf.linalg.global_norm(dense_gradients))
-                tf.summary.scalar("global_norm/embed", tf.linalg.global_norm(embed_vars))
-                tf.summary.scalar("global_norm/var_dense", tf.linalg.global_norm(dense_vars))
-                tf.summary.scalar("global_norm/var_embed", tf.linalg.global_norm(embed_gradients))
+                # tf.summary.scalar("global_norm/dense", tf.linalg.global_norm(dense_gradients))
+                # tf.summary.scalar("global_norm/embed", tf.linalg.global_norm(embed_vars))
+                # tf.summary.scalar("global_norm/var_dense", tf.linalg.global_norm(dense_vars))
+                # tf.summary.scalar("global_norm/var_embed", tf.linalg.global_norm(embed_gradients))
 
                 for i, (variable, gradient) in enumerate(zip(trainable_vars, gradients)):
                     name = variable.name
-                    opt_index = 0 if i in embed_index else 1
-                    m = specs[opt_index]["optimizer"].get_slot(variable, "m")
-                    v = specs[opt_index]["optimizer"].get_slot(variable, "v")
+                    # opt_index = 0 if i in embed_index else 1
+                    # m = specs[opt_index]["optimizer"].get_slot(variable, "m")
+                    # v = specs[opt_index]["optimizer"].get_slot(variable, "v")
 
                     layer_norm = tf.norm(variable)
                     grad_norm = tf.norm(gradient)
-                    m_norm = tf.norm(m)
-                    v_norm = tf.norm(v)
+                    # m_norm = tf.norm(m)
+                    # v_norm = tf.norm(v)
 
                     tf.summary.scalar("layer_norm/" + name, layer_norm)
                     tf.summary.scalar("grad_norm/" + name, grad_norm)
-                    tf.summary.scalar("m_norm/" + name, m_norm)
-                    tf.summary.scalar("v_norm/" + name, v_norm)
+                    # tf.summary.scalar("m_norm/" + name, m_norm)
+                    # tf.summary.scalar("v_norm/" + name, v_norm)
 
-        self.compiled_metrics.update_state(y, y_pred)
+        self.compiled_metrics.update_state(data[1], y_pred)
         for m in self.metrics:
             ret[m.name] = m.result()
         return ret
 
     def cow_clip(self, w, g, ratio=1., ids=None, cnts=None, min_w=0.03, const=False):
         # 算法的核心部分
+        isIdxSlice = False
         if isinstance(g, tf.IndexedSlices):
             # FIXME: This part is not tested
+            isIdxSlice = True
+            indices, shape = g.indices, g.dense_shape
             values = tf.convert_to_tensor(g.values)
             clipnorm = tf.norm(tf.gather(w, g.indices), axis=-1)
         else:
@@ -162,9 +172,11 @@ class Cowclip(Model):
         l2sum_row_safe = tf.where(pred, l2sum_row, tf.ones_like(l2sum_row))
         l2norm_row = tf.sqrt(l2sum_row_safe)
         intermediate = values * tf.expand_dims(clip_t, -1)
+        del g
+        gc.collect()
         g_clip = intermediate / tf.expand_dims(tf.maximum(l2norm_row, clip_t), -1)
 
-        if isinstance(g, tf.IndexedSlices):
-            return tf.IndexedSlices(g_clip, g.indices, g.dense_shape)
+        if isIdxSlice:
+            return tf.IndexedSlices(g_clip, indices, shape)
         else:
             return g_clip
