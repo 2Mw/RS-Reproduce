18cd47f0aad59e9d64be8263ffb088be83e048fe
exp <exp@noreply.com>
2022-11-15 22:39:37 +0800

    Save local changes

--- cf/run/__init__.py
+++ cf/run/__init__.py
@@ -67,9 +67,9 @@ else:
         SELECTED_GPU = get_available_gpu(ins)
         if len(SELECTED_GPU) == 0 or (ASSIGNED_GPU is not None and ASSIGNED_GPU not in SELECTED_GPU):
             print(f'\rCurrently GPUs are busy possibly({get_date()}).', end="")
-            # time.sleep(get_random_num(5, 8))
+            time.sleep(get_random_num(5, 8))
         else:
-            # time.sleep(get_random_num(10, 15))
+            time.sleep(get_random_num(10, 15))
             a = get_available_gpu(ins)
             if ASSIGNED_GPU is None:
                 if len(a) == 0 or len(SELECTED_GPU) == 0:
