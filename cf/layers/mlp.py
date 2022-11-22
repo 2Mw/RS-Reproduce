392b6af40295f214d04f8a3973e7b3c5e03e31e3
2Mw <abd771567@gmail.com>
2022-11-22 09:33:53 +0800

    fix: wrong place of activation for lower version of tf.

--- cf/layers/mlp.py
+++ cf/layers/mlp.py
@@ -1,5 +1,7 @@
 from keras.layers import Dense, Dropout, BatchNormalization, Layer
 from tensorflow import keras
+from cf.layers.activation import PReLU
+import tensorflow as tf
 
 
 class MLP(Layer):
@@ -17,7 +19,12 @@ class MLP(Layer):
         """
         super().__init__(**kwargs)
         self.initializer = initializer if initializer is not None else keras.initializers.glorot_normal
-        self.dnn = [Dense(unit, activation) for unit in units]
+        self.activation = activation
+        if activation is not None and activation.lower() == 'prelu' and tf.__version__ < '2.4.0':
+            self.activation_fn = PReLU()
+        else:
+            self.activation_fn = activation
+        self.dnn = [Dense(unit, self.activation_fn) for unit in units]
         self.dropout = Dropout(dropout)
         self.bn = [BatchNormalization() for unit in units]
         self.use_bn = use_bn
@@ -28,6 +35,8 @@ class MLP(Layer):
         x = inputs
         for i, dense in enumerate(self.dnn):
             x = dense(x)
+            if self.activation.lower() == 'prelu' and tf.__version__ < '2.4.0':
+                x = self.activation_fn(x)
             if self.use_bn:
                 x = self.bn[i](x)
             # dropout 要放在 bn 层后面
